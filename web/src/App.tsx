5813ffacf972828d6a171d7518d50d9bb62a6fc7
Zeno-Huo <bingzhi0019@gmail.com>
2026-08-26 12:47:21 +0800

    backup: web/src/App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import MatchNew from './pages/MatchNew'
+import VideoQuality from './pages/VideoQuality'
+import DetectionTask from './pages/DetectionTask'
+import InstantAnalysis from './pages/InstantAnalysis'
 import Analyzing from './pages/Analyzing'
 import MatchReport from './pages/MatchReport'
+import IdentifyPlayers from './pages/IdentifyPlayers'
+import MyTeam from './pages/MyTeam'
 import NotFound from './pages/NotFound'
 
 export default function App() {
@@ -12,7 +17,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/match/new" element={<MatchNew />} />
+        <Route path="/match/:id/quality" element={<VideoQuality />} />
+        <Route path="/match/:id/instant" element={<InstantAnalysis />} />
+        <Route path="/match/:id/detection" element={<DetectionTask />} />
+        <Route path="/team" element={<MyTeam />} />
         <Route path="/match/:id/analyzing" element={<Analyzing />} />
+        <Route path="/match/:id/identify" element={<IdentifyPlayers />} />
         <Route path="/match/:id" element={<MatchReport />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
